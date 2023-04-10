ecfe4e7ee545fb07b58f636e699020855e7f033e
zjlust <49744465+zjlust@users.noreply.github.com>
2023-04-10 21:45:41 +0800

    Update Change.sol

--- Change.sol
+++ Change.sol
@@ -1,18 +1,15 @@
 pragma solidity ^0.6.12;
 
-contract Deployer {
+contract Challenge {
     address public a;
-    function deploy(bytes memory bytecode,uint salt) internal returns (address){
+    function deploy(bytes memory bytecode,uint salt) internal returns(address) {
         address addr;
-      
         assembly {
           addr := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
         }
-
         return addr;
     }
-    
-    function get(bytes memory code,uint salt)public {
-        a=deploy(code,salt);
+    function get(bytes memory code,uint salt) public {
+        a = deploy(code,salt);
     }
 }
